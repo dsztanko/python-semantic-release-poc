72f121bbffabcb2dcde789274be7326392b38537
dsztanko <szt.dori@gmail.com>
2019-09-06 11:20:55 +0200

    feat: init

---
+++ bin/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pyenv virtualenv python-semantic-release-poc
+pyenv activate python-semantic-release-poc
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="python-semantic-release-poc",
+    version="",
+    packages=["sample"],
+    url="https://github.com/dsztanko/python-semantic-release-poc",
+    license="MIT",
+)
